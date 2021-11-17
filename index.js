4a141ce4be608ea1bad4fa8562268d0fafa53e31
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-17 11:39:37 -0600

    Base

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. IMPORTACIONES
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+// 2. MIDDLEWARES
+app.use(express.static("public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+// 3. RUTAS
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// 4. SERVIDOR
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor activo en puerto ${process.env.PORT}`);
+});
